111ff464ee7bcc1470530f2f993e636564aaf8ec
skarasif <skarasif@gmail.com>
2020-02-02 11:55:43 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld
 public static void main(String args[])
 {
 
-	System.out.println("Farhan Rocks with hooks again B-)");
+	System.out.println("Farhan Rocks with Web Hooks");
 }
 }
