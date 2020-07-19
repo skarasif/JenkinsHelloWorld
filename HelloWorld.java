755dba8d87b5ce4c5a4bb9380886f3a4a093b5f5
skarasif <skarasif@gmail.com>
2020-07-19 20:06:42 +0200

    Update HelloWorld.java
    Updated Print Statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld
 public static void main(String args[])
 {
 
-	System.out.println("Farhan Rocks with Legosvap");
+	System.out.println("Farhan Rocks with Legos Car Truck");
 }
 }
